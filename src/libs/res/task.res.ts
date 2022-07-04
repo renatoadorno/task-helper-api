83a2c8e4b6c07fe55316b9b5812c3874c4a544dd
renato <renatowillian.2000@gmail.com>
2022-07-04 05:40:11 -0300

    ✨ feat: add lib task.res

---
+++ src/libs/res/task.res.ts
@@ -0,0 +1,126 @@
+import { Task } from '../../@types/types/task.types';
+
+export default class LoginRes {
+  // public static ok({
+  //   id, name, email, token,
+  // }: LoginUser) {
+  //   const code = 200;
+  //   return {
+  //     statusCode: code,
+  //     body: {
+  //       id, name, email, token,
+  //     },
+  //   };
+  // }
+
+  public static create({
+    id, createdAt, updatedAt, task, status, userId,
+  }: Task) {
+    const code = 201;
+    return {
+      statusCode: code,
+      body: {
+        id, createdAt, updatedAt, task, status, userId,
+      },
+    };
+  }
+}
+
+// const ok = ({
+//   id,
+//   name,
+//   email,
+//   password,
+// }: User): ResponseUser => {
+//   const code = 200;
+//   return {
+//     statusCode: code,
+//     body: {
+//       id,
+//       name,
+//       email,
+//       password,
+//     },
+//   };
+// };
+
+// const create = ({
+//   id,
+//   name,
+//   email,
+//   password,
+// }: User): ResponseUser => {
+//   const code = 201;
+//   return {
+//     statusCode: code,
+//     body: {
+//       id,
+//       name,
+//       email,
+//       password,
+//     },
+//   };
+// };
+
+// const badRequest = ({
+//   id,
+//   name,
+//   email,
+//   password,
+// }: User): ResponseUser => {
+//   const code = 400;
+//   return {
+//     statusCode: code,
+//     body: {
+//       id,
+//       name,
+//       email,
+//       password,
+//     },
+//   };
+// };
+
+// const unauthorized = ({
+//   id,
+//   name,
+//   email,
+//   password,
+// }: User): ResponseUser => {
+//   const code = 401;
+//   return {
+//     statusCode: code,
+//     body: {
+//       id,
+//       name,
+//       email,
+//       password,
+//     },
+//   };
+// };
+// const notFound = ({
+//   id,
+//   name,
+//   email,
+//   password,
+// }: User): ResponseUser => {
+//   const code = 404;
+//   return {
+//     statusCode: code,
+//     body: {
+//       id,
+//       name,
+//       email,
+//       password,
+//     },
+//   };
+// };
+
+// const response = {
+//   ok,
+//   create,
+//   badRequest,
+//   unauthorized,
+//   notFound,
+// };
+
+// export default response;
