a0fd2f3ed536f5c9e3a15543e0d9c5fb28073a56
renato <renatowillian.2000@gmail.com>
2022-07-04 05:41:50 -0300

    ✨ feat: add model task

---
+++ src/models/TaskModel.ts
@@ -0,0 +1,21 @@
+import { PrismaClient, Task } from '@prisma/client';
+
+export default class TaskModel {
+  private prismaClient;
+
+  constructor() {
+    this.prismaClient = new PrismaClient();
+  }
+
+  public async create(task: string, userId: string): Promise<Task> {
+    const newTask = await this.prismaClient.task.create({
+      data: { task, userId },
+    });
+    return newTask;
+  }
+
+  public async findByUserId(userId: string): Promise<Task[]> {
+    const task = await this.prismaClient.task.findMany({ where: { userId } });
+    return task;
+  }
+}
